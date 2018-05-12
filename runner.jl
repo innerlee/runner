e3edf5f17ea93e336403d5ce0d4f571002a2d06a
lizz <lizz@lab-112.plab.ie.cuhk.edu.hk>
2018-05-13 02:02:02 +0800

    ok

--- runner.jl
+++ runner.jl
@@ -172,7 +172,7 @@ function process_job(job, gpu)
     if length(script) > 2 && script[1] == '"' && script[end] == '"'
         script = strip(script[2:end-1])
     elseif script == ""
-        script = "echo EMPTY FILE!"
+        script = "echo Empty script!\nexit 1"
     end
     lines = split(script, "\n", keep=false)
     lines[end] = "stdbuf -oL " * lines[end]
