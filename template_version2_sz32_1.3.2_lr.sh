6bc3dcad6921891db8234593dae92235f85a1e9e
lizz <363664470@qq.com>
2018-09-17 00:47:51 +0800

    v2 sz32 lr

---
+++ template_version2_sz32_1.3.2_lr.sh
@@ -0,0 +1,2 @@
+cd /home/lizz/dev/pysc2-rl-killer
+python main.py --gpu $GPU --work_dir /home/lizz/population --sz 32 --render 0 --vf_coef 0.25 --ent_coef 0.0001 --discount 0.99 --clip_grads 1 --optimizer adam --beta1 0.9 --beta2 0.999 --step_mul 8 --lr 0.0001 --lr_decay_step 1000 --lr_decay_rate 0.96
